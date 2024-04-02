f7fb86ed36d80844635cce71bc4c72dcf47860c1
Pakarn Pahulo <pakarnpahulo2003@gmail.com>
2024-04-02 19:31:25 +0700

    Add controller template

---
+++ template/controllerTemplate.js
@@ -0,0 +1,74 @@
+/**
+ * @description a template for controller file
+ * replace {LINKENDER} and {OBJECTNAME}
+ */
+
+/**
+ * @description Get 
+ * @route GET /api/v1/read/LINKENDER
+ * @access Public
+ */
+exports.getOBJECTNAME = async (req, res, next) => {
+  try {
+    return res.json({ success: true, data: response });
+  } catch (error) {
+    console.log(error.stack);
+    return res.json({ success: false });
+  }
+};
+
+/**
+ * @description create 
+ * @route POST /api/v1/create/LINKENDER
+ * @access Private
+ */
+exports.postOBJECTNAME = (req, res, next) => {
+  try {
+    return res.json({ success: true });
+  } catch (error) {
+    console.log(error.stack);
+    return res.json({ success: false });
+  }
+};
+
+/**
+ * @description update 
+ * @route PUT /api/v1/update/LINKENDER
+ * @access Private
+ */
+exports.putOBJECTNAME = (req, res, next) => {
+  try {
+    return res.json({ success: true });
+  } catch (error) {
+    console.log(error.stack);
+    return res.json({ success: false });
+  }
+};
+
+/**
+ * @description delete 
+ * @route DELETE /api/v1/update/LINKENDER
+ * @access Private
+ */
+exports.deleteOBJECTNAME = (req, res, next) => {
+  try {
+    return res.json({ success: true });
+  } catch (error) {
+    console.log(error.stack);
+    return res.json({ success: false });
+  }
+};
+
+/**
+ * @description refetch 
+ * @route PATCH /api/v1/refetch/LINKENDER
+ * @access Private
+ */
+exports.refetchOBJECTNAME = async (req, res, next) => {
+  try {
+    return res.json({ success: true, data: response });
+  } catch (error) {
+    console.log(error.stack);
+    return res.json({ success: false });
+  }
+};

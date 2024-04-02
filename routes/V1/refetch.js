19054c86adfe86cbd41c8b6da09e2cbb421be9bb
Pakarn Pahulo <pakarnpahulo2003@gmail.com>
2024-04-02 20:11:53 +0700

    Add routes for refetching fund benchmark and fact sheet

--- routes/V1/refetch.js
+++ routes/V1/refetch.js
@@ -1,10 +1,14 @@
 const { Router } = require('express');
 const { refetchMutualFundEntity } = require("../../controllers/V1/MutualFundEntity");
 const { refetchMutualFunds } = require('../../controllers/V1/MutualFundsGeneral');
+const { refetchFundBenchmark } = require('../../controllers/V1/MutualFundBenchMark');
+const { refetchFundFactSheet } = require('../../controllers/V1/MutualFundFactSheet');
 const router = Router();
 
 router.route("/entities").patch(refetchMutualFundEntity);
+router.route('/fundfactsheet').patch(refetchFundFactSheet);
 router.route('/funds').patch(refetchMutualFunds);
+router.route('/benchmark').patch(refetchFundBenchmark);
 
 router.get("/", (req, res) => {
   res.status(200);
